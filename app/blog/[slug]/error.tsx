23b79276b519f82f91a91b53ddd86827b634361c
AirOne01 <popgthyrd@gmail.com>
2023-03-24 17:15:47 +0100

    feat(blog): add an error page

--- app/blog/[slug]/error.tsx
+++ app/blog/[slug]/error.tsx
@@ -1,17 +1,18 @@
-import getSplash from "app/loadingSplash";
-import Spinner from "./Spinner";
+"use client";
 
-export default function Loading() {
+import getSplash from "app/errorSplash";
+
+const Error = () => {
   return (
     <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center">
       <div className="card z-50 w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title">
-            <Spinner /> Loading...
-          </h2>
+          <h2 className="card-title">There was an error :-/</h2>
           <p className="opacity-25">{getSplash()}</p>
         </div>
       </div>
     </div>
   );
 }
+
+export default Error;
