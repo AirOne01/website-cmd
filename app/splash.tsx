e7f88c513aba5f7b9de50727ebdf1e43c8fdacf3
AirOne01 <popgthyrd@gmail.com>
2023-03-24 16:54:32 +0100

    fix(blog): separate loading and error splashes

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,10 +1,13 @@
-const splashes = [
+const loadingSplashes = [
   "So long, and thanks for all the fish.",
-  "I'm sorry, Dave. I'm afraid I can't do that.",
-  "16 / 16 / 16 / 16 / 16",
   "Tuturu~",
 ];
 
-const getSplash = () => splashes[Math.floor(Math.random() * splashes.length)];
+const errorSplashes = [
+  "I'm sorry, Dave. I'm afraid I can't do that.",
+  "16 / 16 / 16 / 16 / 16",
+];
+
+const getSplash = () => loadingSplashes[Math.floor(Math.random() * loadingSplashes.length)];
 
 export default getSplash;
