8fe78a5cbb3b09a178c552f1c16d6295c6c2684b
Erwann Lagouche <popGthyrd@gmail.com>
2023-03-27 04:17:07 +0200

    Revert "Revert "Mobile navigation""
    This reverts commit ccdcfc565520f01bfd680163b0715d24606d6f1f.

--- app/blog/loading.tsx
+++ app/blog/loading.tsx
@@ -1,17 +1,5 @@
-import { getLoadingSplash } from "app/splashes";
-import Spinner from "./[slug]/Spinner";
+import { LoadingModal } from "app/Loading";
 
-export default function Loading() {
-  return (
-    <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center">
-      <div className="card z-50 w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">
-            <Spinner /> Loading...
-          </h2>
-          <p className="opacity-25">{getLoadingSplash()}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+const Loading = () => <LoadingModal />;
+
+export default Loading;
