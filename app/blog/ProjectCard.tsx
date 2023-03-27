4e734de58ec1a80e4f97e8d98a2fd01a74e31c2c
AirOne01 <popgthyrd@gmail.com>
2023-03-27 03:24:43 +0200

    style: switch from 6 characters long colors to 3

--- app/blog/ProjectCard.tsx
+++ app/blog/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard = ({
   const currentDate = new Date(Date.now());
 
   return (
-    <div className="card w-96 border border-[#333333] bg-black">
+    <div className="card w-96 border border-[#333] bg-black">
       <figure className="aspect-video">
         <Image
           src={`/posts-images/covers/${image}`}
@@ -23,11 +23,11 @@ const ProjectCard = ({
           alt={title}
         />
       </figure>
-      <div className="card-body border-t border-[#333333]">
+      <div className="card-body border-t border-[#333]">
         <Link href={`blog/${slug}`} className="flex w-full flex-row">
           <h2 className="text-lg font-bold">{title}</h2>
           {addDays(mdDate, 30) > currentDate ? (
-            <div className="flex justify-center items-center rounded-full border-2 border-[#333333] px-2 ml-4 text-sm">
+            <div className="flex justify-center items-center rounded-full border-2 border-[#333] px-2 ml-4 text-sm">
               NEW
             </div>
           ) : (
--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -7,7 +7,7 @@ const BlogLayout = ({ children }: { children: React.ReactNode }) => {
       <header className="flex w-full justify-between p-4">
         <section className="whitespace-pre-line leading-tight">
           <Link href={"/"} className="blur-bold">Erwann lagouche</Link><br />
-          <span className="text-[#555555] text-sm">Lyon, France</span>
+          <span className="text-[#555] text-sm">Lyon, France</span>
         </section>
         <nav className="pr-4">
           <Link href={"/"} className="blur-bold">
@@ -21,7 +21,7 @@ const BlogLayout = ({ children }: { children: React.ReactNode }) => {
       <main className="flex min-h-full grow flex-col items-center justify-center">
         {children}
       </main>
-      <footer className="flex justify-between border-t border-[#333333] p-4">
+      <footer className="flex justify-between border-t border-[#333] p-4">
         <p>Copyright © 2023 Erwann Lagouche - All right reserved</p>
         <Link href={"https://github.com/AirOne01"} target="_blank">
           <Image
