b6c7efd63642933b06d16501e57d7d3ef42a5729
AirOne01 <popgthyrd@gmail.com>
2023-03-31 14:17:04 +0200

    feat: add skills

---
+++ app/Section3.tsx
@@ -0,0 +1,69 @@
+const Section3 = () => (
+  <section className="flex h-screen w-full snap-center flex-col items-center justify-center bg-black">
+    <h1 className="pb-8 text-4xl font-bold">Skills</h1>
+    <main className="flex w-2/3 flex-col gap-4">
+      <div className="min-h-24 flex border border-[#333]">
+        <div className="flex h-full items-center border-r border-[#333] px-4 font-bold">
+          <h1>Information systems</h1>
+        </div>
+        <p className="p-2 font-normal">
+          Throughout my academic and professional career, I have had the
+          occasion to study information systems and databases. As a result of
+          this experience, I have gained a solid understanding of how these
+          systems operate and the role they play in modern business
+          environments. I have also learned how to design, implement, and manage
+          databases using a variety of technologies and tools, including SQL,
+          Oracle, and MongoDB. This knowledge has proved invaluable in my work,
+          allowing me to create efficient and effective database solutions that
+          support the needs of businesses and organizations. I am proud to
+          showcase this experience in my portfolio, which highlights my skills
+          and expertise in information systems and databases.
+        </p>
+      </div>
+      <div className="min-h-24 flex border border-[#333]">
+        <div className="flex h-full items-center border-r border-[#333] px-4 font-bold">
+          <h1>CI/CD</h1>
+        </div>
+        <p className="p-2 font-normal">
+          As a software developer, I have had the opportunity to gain valuable
+          experience in Continuous Integration/Continuous Deployment (CI/CD).
+          Throughout my career, I have worked with a variety of tools and
+          technologies to streamline the development process, including Jenkins,
+          CircleCI, and TravisCI. I have also developed a strong understanding
+          of the benefits of CI/CD, including faster release cycles, reduced
+          risk, and improved quality assurance. Through my work in this area, I
+          have become skilled in automating build, test, and deployment
+          processes, allowing for more efficient development and delivery of
+          software products. I am proud to showcase this experience in my
+          portfolio, which demonstrates my ability to design and implement
+          effective CI/CD pipelines and streamline software development
+          processes.
+        </p>
+      </div>
+      <div className="min-h-24 flex border border-[#333]">
+        <div className="flex h-full items-center border-r border-[#333] px-4 font-bold">
+          <h1>Web development</h1>
+        </div>
+        <p className="p-2 font-normal">
+          As a web developer, I have had the opportunity to work on a wide range
+          of projects using various web development technologies and frameworks.
+          My experience with HTML, CSS, and JavaScript has allowed me to create
+          engaging and dynamic web applications that are both visually appealing
+          and functional. Additionally, I have worked extensively with popular
+          frameworks such as React, Angular, and Vue.js to create scalable and
+          responsive web applications. I am also skilled in server-side
+          development using technologies such as Node.js, PHP, and Ruby on
+          Rails, allowing me to build robust web applications that are
+          performant and secure. Through my experience with web development, I
+          have developed a strong understanding of best practices and industry
+          standards, enabling me to create web applications that meet the needs
+          of clients and users. I am proud to showcase this experience in my
+          portfolio, which demonstrates my skills and expertise in web
+          development.
+        </p>
+      </div>
+    </main>
+  </section>
+);
+
+export default Section3;
